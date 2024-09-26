c13c2e251c697f156566f24815f015a5a5dccae7
Julian <jhunsicker@united-internet.de>
2024-09-26 15:33:55 +0200

    fix more dependencies

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+# Error on all systems runtime dependency libev not foundcd
+
 # Function to install dependencies for Debian-based distributions
 install_debian() {
     sudo apt update
@@ -16,7 +19,7 @@ install_fedora() {
     sudo dnf install dbus-devel gcc git libconfig-devel libdrm-devel libev-devel libX11-devel libX11-xcb libXext-devel \
     libxcb-devel libGL-devel libEGL-devel libepoxy-devel meson ninja-build pcre2-devel pixman-devel uthash-devel \
     xcb-util-image-devel xcb-util-renderutil-devel xorg-x11-proto-devel xcb-util-devel cmake alsa-utils thunar feh \
-    flameshot dunst libXft rofi alacritty unzip
+    flameshot dunst libXft rofi alacritty unzip libXft-devel imlib2-devel libXinerama-devel
 }
 
 # Function to install dependencies for Arch-based distributions
@@ -201,9 +204,10 @@ configure_backgrounds() {
 }
 
 slstatus() {
-    cd .slstatus || exit
+    cd slstatus || exit
     make
     sudo make install
+    cd ..
 }
 
 # Call the function
