a94330323baeab805da5d732298ec54e118dc24b
sxyql <julian.hunsicker@tutanota.com>
2024-10-07 17:56:21 +0200

    added sddm install and fix starship and zoxide error

--- setup.sh
+++ setup.sh
@@ -7,6 +7,12 @@ log_error() {
     echo "[ERROR] $1" >&2
 }
 
+# Check if the script is run as root
+if [ "$EUID" -ne 0 ]; then
+    log_error "Please run as root"
+    exit 1
+fi
+
 # Trap errors and perform cleanup
 trap 'log_error "An error occurred. Exiting..."; exit 1' ERR
 
@@ -14,15 +20,20 @@ trap 'log_error "An error occurred. Exiting..."; exit 1' ERR
 install_debian() {
     sudo apt update || { log_error "Failed to update package list"; exit 1; }
     sudo apt full-upgrade -y || { log_error "Failed to upgrade packages"; exit 1; }
-    sudo apt install libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev \
+    sudo apt install -y libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev \
     libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev \
     libxcb-dpms0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev \
     libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev meson ninja-build \
     uthash-dev cmake libxft-dev libimlib2-dev libxinerama-dev libxcb-res0-dev alsa-utils thunar feh flameshot dunst \
-    rofi alacritty unzip wget curl bash-completion lightdm fastfetch starship \
-    zoxide|| { log_error "Failed to install dependencies"; }
-    sudo apt autoremove || { log_error "Failed to remove unused packages"; }
-    sudo apt autoclean || { log_error "Failed to clean package cache"; }
+    rofi alacritty unzip wget curl bash-completion lightdm fastfetch || { log_error "Failed to install dependencies"; exit 1; }
+    sudo apt autoremove -y || { log_error "Failed to remove unused packages"; exit 1; }
+    sudo apt autoclean || { log_error "Failed to clean package cache"; exit 1; }
+
+    # Install zoxide
+    curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash || { log_error "Failed to install zoxide"; exit 1; }
+
+    # Install starship
+    curl -sS https://starship.rs/install.sh | sh || { log_error "Failed to install starship"; exit 1; }
 }
 
 
@@ -33,10 +44,17 @@ install_redhat() {
     libXext-devel libxcb-devel libGL-devel libEGL-devel libepoxy-devel meson ninja-build pcre2-devel pixman-devel \
     uthash-devel xcb-util-image-devel xcb-util-renderutil-devel xorg-x11-proto-devel xcb-util-devel cmake \
     libxft-devel libimlib2-devel libxinerama-devel libxcb-res0-devel alacritty thunar feh flameshot dunst rofi \
-    alsa-utils btop htop trash-cli fastfetch starship zoxide || { log_error "Failed to install dependencies"; }
+    alsa-utils btop htop trash-cli fastfetch || { log_error "Failed to install dependencies"; }
+    # Install zoxide
+    curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash || { log_error "Failed to install zoxide"; exit 1; }
+
+    # Install starship
+    curl -sS https://starship.rs/install.sh | sh || { log_error "Failed to install starship"; exit 1; }
+
     sudo yum autoremove || { log_error "Failed to remove unused packages"; }
     sudo yum clean all || { log_error "Failed to clean package cache"; }
 }
+
 
 # Function to install dependencies for Fedora
 install_fedora() {
@@ -46,8 +64,13 @@ install_fedora() {
     libXext-devel libxcb-devel libGL-devel libEGL-devel libepoxy-devel libXft-devel imlib2-devel \
     libXinerama-devel  meson ninja-build pcre2-devel pixman-devel uthash-devel xcb-util-image-devel \
     xcb-util-renderutil-devel xorg-x11-proto-devel xcb-util-devel cmake alsa-utils thunar feh flameshot dunst \
-    libXft rofi alacritty unzip wget curl bash-completion btop htop trash-cli fastfetch  starship \
-    zoxide || { log_error "Failed to install dependencies"; }
+    libXft rofi alacritty unzip wget curl bash-completion btop htop trash-cli fastfetch || { log_error "Failed to install dependencies"; }
+    # Install zoxide
+    curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+
+    # Install starship
+    curl -sS https://starship.rs/install.sh | sh
+
     sudo dnf autoremove || { log_error "Failed to remove unused packages"; }
     sudo dnf clean all || { log_error "Failed to clean package cache"; }
 }
@@ -59,6 +82,11 @@ install_arch() {
     pixman uthash xcb-util-image xcb-util-renderutil xorgproto cmake libxft imlib2 libxinerama xcb-util-wm xorg-xev \
     xorg-xbacklight alsa-utils thunar feh flameshot dunst rofi alacritty unzip wget curl sddm btop htop \
     bash-completion trash-cli fastfetch || { log_error "Failed to install dependencies"; exit 1; }
+    # Install zoxide
+    sudo pacman -S zoxide
+
+    # Install starship
+    sudo pacman -S starship
     sudo pacman -Rns "$(pacman -Qdtq)" || { log_error "Failed to remove unused packages"; }
     sudo pacman -Sc --noconfirm || { log_error "Failed to clean package cache"; exit 1; }
 }
@@ -252,6 +280,12 @@ install_dwm() {
     cd ..
 }
 
+install_sddm() {
+    sudo pacman -S sddm
+    sudo systemctl enable sddm
+    sudo systemctl start sddm
+}
+
 # Call the functions
 install_nerd_font
 clone_config_folders
@@ -260,5 +294,6 @@ configure_backgrounds
 slstatus
 copybashfile
 install_dwm
+install_sddm
 
 echo "All dependencies installed successfully."
